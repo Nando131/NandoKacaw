9bfa7b51c96c051c5bd45a860b28ef734dc40d51
Nando131 <Nandoher00@gmail.com>
2024-11-12 20:29:09 +0700

    Added ToggleName component

---
+++ Hernando-Assignment-18/src/App.jsx
@@ -0,0 +1,21 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React, { useState } from 'react';
+
+  function App() {
+    const [showName, setShowName] = useState(false);
+
+    const toggleName = () => {
+      setShowName(!showName);
+    };
+
+    return (
+      <div>
+        <button onClick={toggleName}>Show/Hide Name</button>
+        {showName && <p>Your Name</p>}
+      </div>
+    );
+  }
+
+  export default App;
