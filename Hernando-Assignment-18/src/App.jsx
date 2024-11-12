1a703e62591acdbf7e83f1a6b24332763f8c3b6f
Nando131 <Nandoher00@gmail.com>
2024-11-12 20:59:24 +0700

    Added ToggleName component

--- Hernando-Assignment-18/src/App.jsx
+++ Hernando-Assignment-18/src/App.jsx
@@ -12,29 +12,8 @@ function App() {
 
     return (
         <div>
-            <button 
-                style={{
-                    backgroundColor: '#4CAF50', /* Green */
-                    border: 'none',
-                    color: 'white',
-                    padding: '15px 32px',
-                    textAlign: 'center',
-                    textDecoration: 'none',
-                    display: 'inline-block',
-                    fontSize: '16px',
-                    margin: '4px 2px',
-                    cursor: 'pointer'
-                }}
-                onClick={handleClick}>
-                Tampilkan
-            </button>
-            {isShown && (
-                <p style={{
-                    color: 'blue',
-                    fontSize: '18px',
-                    fontWeight: 'bold'
-                }}>Your Name</p>
-            )}
+            <button onClick={handleClick}>Tampilkan</button>
+            {isShown && <p>Your Name</p>}
         </div>
     );
 }
